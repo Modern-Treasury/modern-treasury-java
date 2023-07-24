5f23eb2d1ec6196e0023ad6b0d76dc280bcf1844
annie.chen@moderntreasury.com <39141411+chen-annie@users.noreply.github.com>
2023-07-24 13:11:19 -0700

    AccountConnection -> AccountCollection

--- modern-treasury-java-core/src/test/kotlin/com/moderntreasury/api/models/AccountConnectionFlowTest.kt
+++ modern-treasury-java-core/src/test/kotlin/com/moderntreasury/api/models/AccountConnectionFlowTest.kt
@@ -4,38 +4,38 @@ import java.time.OffsetDateTime
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class AccountConnectionFlowTest {
+class AccountCollectionFlowTest {
 
     @Test
-    fun createAccountConnectionFlow() {
-        val accountConnectionFlow =
-            AccountConnectionFlow.builder()
+    fun createAccountCollectionFlow() {
+        val accountCollectionFlow =
+            AccountCollectionFlow.builder()
                 .counterpartyId("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
-                .paymentTypes(listOf(AccountConnectionFlow.PaymentType.ACH))
+                .paymentTypes(listOf(AccountCollectionFlow.PaymentType.ACH))
                 .id("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
                 .clientToken("string")
                 .createdAt(OffsetDateTime.parse("2019-12-27T18:11:19.117Z"))
                 .externalAccountId("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
                 .liveMode(true)
                 .object_("string")
-                .status(AccountConnectionFlow.Status.CANCELLED)
+                .status(AccountCollectionFlow.Status.CANCELLED)
                 .updatedAt(OffsetDateTime.parse("2019-12-27T18:11:19.117Z"))
                 .build()
-        assertThat(accountConnectionFlow).isNotNull
-        assertThat(accountConnectionFlow.counterpartyId())
+        assertThat(accountCollectionFlow).isNotNull
+        assertThat(accountCollectionFlow.counterpartyId())
             .isEqualTo("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
-        assertThat(accountConnectionFlow.paymentTypes())
-            .containsExactly(AccountConnectionFlow.PaymentType.ACH)
-        assertThat(accountConnectionFlow.id()).contains("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
-        assertThat(accountConnectionFlow.clientToken()).contains("string")
-        assertThat(accountConnectionFlow.createdAt())
+        assertThat(accountCollectionFlow.paymentTypes())
+            .containsExactly(AccountCollectionFlow.PaymentType.ACH)
+        assertThat(accountCollectionFlow.id()).contains("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
+        assertThat(accountCollectionFlow.clientToken()).contains("string")
+        assertThat(accountCollectionFlow.createdAt())
             .contains(OffsetDateTime.parse("2019-12-27T18:11:19.117Z"))
-        assertThat(accountConnectionFlow.externalAccountId())
+        assertThat(accountCollectionFlow.externalAccountId())
             .contains("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e")
-        assertThat(accountConnectionFlow.liveMode()).contains(true)
-        assertThat(accountConnectionFlow.object_()).contains("string")
-        assertThat(accountConnectionFlow.status()).contains(AccountConnectionFlow.Status.CANCELLED)
-        assertThat(accountConnectionFlow.updatedAt())
+        assertThat(accountCollectionFlow.liveMode()).contains(true)
+        assertThat(accountCollectionFlow.object_()).contains("string")
+        assertThat(accountCollectionFlow.status()).contains(AccountCollectionFlow.Status.CANCELLED)
+        assertThat(accountCollectionFlow.updatedAt())
             .contains(OffsetDateTime.parse("2019-12-27T18:11:19.117Z"))
     }
 }
