57de8a8fb1e844b138479cd79610be1b765fbc81
mtengbot <eng.bot@moderntreasury.com>
2026-07-24 22:31:04 +0000

    seal

--- modern-treasury-java-core/src/test/kotlin/com/moderntreasury/api/services/async/LegalEntityServiceAsyncTest.kt
+++ modern-treasury-java-core/src/test/kotlin/com/moderntreasury/api/services/async/LegalEntityServiceAsyncTest.kt
@@ -17,12 +17,14 @@ import com.moderntreasury.api.models.ThirdPartyVerification
 import com.moderntreasury.api.models.WealthAndEmploymentDetails
 import java.time.LocalDate
 import java.time.OffsetDateTime
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(TestServerExtension::class)
 internal class LegalEntityServiceAsyncTest {
 
+    @Disabled("Mock server doesn't generate valid example responses for recursive schemas")
     @Test
     fun create() {
         val client =
@@ -456,6 +458,7 @@ internal class LegalEntityServiceAsyncTest {
         legalEntity.validate()
     }
 
+    @Disabled("Mock server doesn't generate valid example responses for recursive schemas")
     @Test
     fun retrieve() {
         val client =
@@ -472,6 +475,7 @@ internal class LegalEntityServiceAsyncTest {
         legalEntity.validate()
     }
 
+    @Disabled("Mock server doesn't generate valid example responses for recursive schemas")
     @Test
     fun update() {
         val client =
@@ -652,6 +656,7 @@ internal class LegalEntityServiceAsyncTest {
         legalEntity.validate()
     }
 
+    @Disabled("Mock server doesn't generate valid example responses for recursive schemas")
     @Test
     fun list() {
         val client =
