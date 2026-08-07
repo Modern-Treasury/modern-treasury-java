7a6be873551f59829d3b12c3487ac8a4d293e12b
mtengbot <eng.bot@moderntreasury.com>
2026-08-07 21:33:34 +0000

    Build SDK
    Stainless-Generated-From: 9e27786d27d0bc8effe5b44b90371806b35bc679

--- modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/ReturnObject.kt
+++ modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/ReturnObject.kt
@@ -3205,6 +3205,8 @@ private constructor(
                 val WESTERN_ALLIANCE_WIRE_CONFIRMATION_NUMBER =
                     of("western_alliance_wire_confirmation_number")
 
+                @JvmField val WISE_TRANSFER_ID = of("wise_transfer_id")
+
                 @JvmStatic fun of(value: String) = ReferenceNumberType(JsonField.of(value))
             }
 
@@ -3299,6 +3301,7 @@ private constructor(
                 WESTERN_ALLIANCE_PAYMENT_ID,
                 WESTERN_ALLIANCE_TRANSACTION_ID,
                 WESTERN_ALLIANCE_WIRE_CONFIRMATION_NUMBER,
+                WISE_TRANSFER_ID,
             }
 
             /**
@@ -3402,6 +3405,7 @@ private constructor(
                 WESTERN_ALLIANCE_PAYMENT_ID,
                 WESTERN_ALLIANCE_TRANSACTION_ID,
                 WESTERN_ALLIANCE_WIRE_CONFIRMATION_NUMBER,
+                WISE_TRANSFER_ID,
                 /**
                  * An enum member indicating that [ReferenceNumberType] was instantiated with an
                  * unknown value.
@@ -3511,6 +3515,7 @@ private constructor(
                     WESTERN_ALLIANCE_TRANSACTION_ID -> Value.WESTERN_ALLIANCE_TRANSACTION_ID
                     WESTERN_ALLIANCE_WIRE_CONFIRMATION_NUMBER ->
                         Value.WESTERN_ALLIANCE_WIRE_CONFIRMATION_NUMBER
+                    WISE_TRANSFER_ID -> Value.WISE_TRANSFER_ID
                     else -> Value._UNKNOWN
                 }
 
@@ -3618,6 +3623,7 @@ private constructor(
                     WESTERN_ALLIANCE_TRANSACTION_ID -> Known.WESTERN_ALLIANCE_TRANSACTION_ID
                     WESTERN_ALLIANCE_WIRE_CONFIRMATION_NUMBER ->
                         Known.WESTERN_ALLIANCE_WIRE_CONFIRMATION_NUMBER
+                    WISE_TRANSFER_ID -> Known.WISE_TRANSFER_ID
                     else ->
                         throw ModernTreasuryInvalidDataException(
                             "Unknown ReferenceNumberType: $value"
