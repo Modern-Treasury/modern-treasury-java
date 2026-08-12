34e312b4f879cf000400f0fa2255a696e0866737
mtengbot <eng.bot@moderntreasury.com>
2026-08-12 14:56:34 +0000

    seal

--- modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/BulkRequestListParams.kt
+++ modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/BulkRequestListParams.kt
@@ -539,8 +539,6 @@ private constructor(
 
             @JvmField val TRANSACTION_LINE_ITEM = of("transaction_line_item")
 
-            @JvmField val ENTITY_LINK = of("entity_link")
-
             @JvmStatic fun of(value: String) = ResourceType(JsonField.of(value))
         }
 
@@ -552,7 +550,6 @@ private constructor(
             EXPECTED_PAYMENT,
             TRANSACTION,
             TRANSACTION_LINE_ITEM,
-            ENTITY_LINK,
         }
 
         /**
@@ -571,7 +568,6 @@ private constructor(
             EXPECTED_PAYMENT,
             TRANSACTION,
             TRANSACTION_LINE_ITEM,
-            ENTITY_LINK,
             /**
              * An enum member indicating that [ResourceType] was instantiated with an unknown value.
              */
@@ -593,7 +589,6 @@ private constructor(
                 EXPECTED_PAYMENT -> Value.EXPECTED_PAYMENT
                 TRANSACTION -> Value.TRANSACTION
                 TRANSACTION_LINE_ITEM -> Value.TRANSACTION_LINE_ITEM
-                ENTITY_LINK -> Value.ENTITY_LINK
                 else -> Value._UNKNOWN
             }
 
@@ -614,7 +609,6 @@ private constructor(
                 EXPECTED_PAYMENT -> Known.EXPECTED_PAYMENT
                 TRANSACTION -> Known.TRANSACTION
                 TRANSACTION_LINE_ITEM -> Known.TRANSACTION_LINE_ITEM
-                ENTITY_LINK -> Known.ENTITY_LINK
                 else -> throw ModernTreasuryInvalidDataException("Unknown ResourceType: $value")
             }
 
