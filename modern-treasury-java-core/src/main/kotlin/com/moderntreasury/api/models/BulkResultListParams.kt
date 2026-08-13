8c3ffe3a7c46ad4289b9dd07d3a45c6692179ae4
mtengbot <eng.bot@moderntreasury.com>
2026-08-13 20:11:00 +0000

    Build SDK
    Stainless-Generated-From: f9ec21f34ee1555260508dcd362859e475b9806b

--- modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/BulkResultListParams.kt
+++ modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/BulkResultListParams.kt
@@ -314,8 +314,6 @@ private constructor(
 
             @JvmField val TRANSACTION = of("transaction")
 
-            @JvmField val ENTITY_LINK = of("entity_link")
-
             @JvmField val TRANSACTION_LINE_ITEM = of("transaction_line_item")
 
             @JvmField val BULK_ERROR = of("bulk_error")
@@ -330,7 +328,6 @@ private constructor(
             LEDGER_TRANSACTION,
             EXPECTED_PAYMENT,
             TRANSACTION,
-            ENTITY_LINK,
             TRANSACTION_LINE_ITEM,
             BULK_ERROR,
         }
@@ -350,7 +347,6 @@ private constructor(
             LEDGER_TRANSACTION,
             EXPECTED_PAYMENT,
             TRANSACTION,
-            ENTITY_LINK,
             TRANSACTION_LINE_ITEM,
             BULK_ERROR,
             /**
@@ -373,7 +369,6 @@ private constructor(
                 LEDGER_TRANSACTION -> Value.LEDGER_TRANSACTION
                 EXPECTED_PAYMENT -> Value.EXPECTED_PAYMENT
                 TRANSACTION -> Value.TRANSACTION
-                ENTITY_LINK -> Value.ENTITY_LINK
                 TRANSACTION_LINE_ITEM -> Value.TRANSACTION_LINE_ITEM
                 BULK_ERROR -> Value.BULK_ERROR
                 else -> Value._UNKNOWN
@@ -395,7 +390,6 @@ private constructor(
                 LEDGER_TRANSACTION -> Known.LEDGER_TRANSACTION
                 EXPECTED_PAYMENT -> Known.EXPECTED_PAYMENT
                 TRANSACTION -> Known.TRANSACTION
-                ENTITY_LINK -> Known.ENTITY_LINK
                 TRANSACTION_LINE_ITEM -> Known.TRANSACTION_LINE_ITEM
                 BULK_ERROR -> Known.BULK_ERROR
                 else -> throw ModernTreasuryInvalidDataException("Unknown EntityType: $value")
