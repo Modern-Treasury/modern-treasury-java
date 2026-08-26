6de44a0a3efe547386dac026c55e780e9e534f32
mtengbot <eng.bot@moderntreasury.com>
2026-08-26 20:08:47 +0000

    seal

--- modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/PaymentOrderUpdateParams.kt
+++ modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/PaymentOrderUpdateParams.kt
@@ -214,10 +214,9 @@ private constructor(
         body.originatingPartyAddress()
 
     /**
-     * If present, this will replace your default company name on receiver's bank statement. For
-     * ACH, only the first 16 characters of this string will be used. This must be a name the
-     * account holder legally operates under, such as a DBA. Do not use it to originate on behalf of
-     * another person or business, including an end customer.
+     * If present, this will replace your default company name on receiver's bank statement. This
+     * field can only be used for ACH payments currently. For ACH, only the first 16 characters of
+     * this string will be used. Any additional characters will be truncated.
      *
      * @throws ModernTreasuryInvalidDataException if the JSON field has an unexpected type (e.g. if
      *   the server responded with an unexpected value).
@@ -1102,10 +1101,9 @@ private constructor(
             }
 
         /**
-         * If present, this will replace your default company name on receiver's bank statement. For
-         * ACH, only the first 16 characters of this string will be used. This must be a name the
-         * account holder legally operates under, such as a DBA. Do not use it to originate on
-         * behalf of another person or business, including an end customer.
+         * If present, this will replace your default company name on receiver's bank statement.
+         * This field can only be used for ACH payments currently. For ACH, only the first 16
+         * characters of this string will be used. Any additional characters will be truncated.
          */
         fun originatingPartyName(originatingPartyName: String?) = apply {
             body.originatingPartyName(originatingPartyName)
@@ -2010,10 +2008,9 @@ private constructor(
             originatingPartyAddress.getOptional("originating_party_address")
 
         /**
-         * If present, this will replace your default company name on receiver's bank statement. For
-         * ACH, only the first 16 characters of this string will be used. This must be a name the
-         * account holder legally operates under, such as a DBA. Do not use it to originate on
-         * behalf of another person or business, including an end customer.
+         * If present, this will replace your default company name on receiver's bank statement.
+         * This field can only be used for ACH payments currently. For ACH, only the first 16
+         * characters of this string will be used. Any additional characters will be truncated.
          *
          * @throws ModernTreasuryInvalidDataException if the JSON field has an unexpected type (e.g.
          *   if the server responded with an unexpected value).
@@ -3043,9 +3040,8 @@ private constructor(
 
             /**
              * If present, this will replace your default company name on receiver's bank statement.
-             * For ACH, only the first 16 characters of this string will be used. This must be a
-             * name the account holder legally operates under, such as a DBA. Do not use it to
-             * originate on behalf of another person or business, including an end customer.
+             * This field can only be used for ACH payments currently. For ACH, only the first 16
+             * characters of this string will be used. Any additional characters will be truncated.
              */
             fun originatingPartyName(originatingPartyName: String?) =
                 originatingPartyName(JsonField.ofNullable(originatingPartyName))
