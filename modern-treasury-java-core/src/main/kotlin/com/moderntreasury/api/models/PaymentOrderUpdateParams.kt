d02a0f17bbb49db11aff7ccbfa1cfaa9a3cc1525
mtengbot <eng.bot@moderntreasury.com>
2026-09-04 21:47:17 +0000

    seal

--- modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/PaymentOrderUpdateParams.kt
+++ modern-treasury-java-core/src/main/kotlin/com/moderntreasury/api/models/PaymentOrderUpdateParams.kt
@@ -6489,8 +6489,6 @@ private constructor(
 
                     @JvmField val SOLANA_ADDRESS = of("solana_address")
 
-                    @JvmField val WALLET_ADDRESS = of("wallet_address")
-
                     @JvmStatic fun of(value: String) = AccountNumberType(JsonField.of(value))
                 }
 
@@ -6510,7 +6508,6 @@ private constructor(
                     POLYGON_ADDRESS,
                     SG_NUMBER,
                     SOLANA_ADDRESS,
-                    WALLET_ADDRESS,
                 }
 
                 /**
@@ -6539,7 +6536,6 @@ private constructor(
                     POLYGON_ADDRESS,
                     SG_NUMBER,
                     SOLANA_ADDRESS,
-                    WALLET_ADDRESS,
                     /**
                      * An enum member indicating that [AccountNumberType] was instantiated with an
                      * unknown value.
@@ -6570,7 +6566,6 @@ private constructor(
                         POLYGON_ADDRESS -> Value.POLYGON_ADDRESS
                         SG_NUMBER -> Value.SG_NUMBER
                         SOLANA_ADDRESS -> Value.SOLANA_ADDRESS
-                        WALLET_ADDRESS -> Value.WALLET_ADDRESS
                         else -> Value._UNKNOWN
                     }
 
@@ -6599,7 +6594,6 @@ private constructor(
                         POLYGON_ADDRESS -> Known.POLYGON_ADDRESS
                         SG_NUMBER -> Known.SG_NUMBER
                         SOLANA_ADDRESS -> Known.SOLANA_ADDRESS
-                        WALLET_ADDRESS -> Known.WALLET_ADDRESS
                         else ->
                             throw ModernTreasuryInvalidDataException(
                                 "Unknown AccountNumberType: $value"
